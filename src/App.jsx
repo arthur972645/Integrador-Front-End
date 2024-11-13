88aa005faa455f23745c8b7d0a6cacd3874aa7fe
arthur972645 <arthurall.1973@gmail.com>
2024-11-13 12:15:04 -0300

    Começando o dashebord do projeto integrador

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import PaginaIncial from "./Pages/Pagina-Inicial/Pagina-Inicial.jsx"
-import PaginaAcomodacoes from './Pages/Pagina-Acomodações/Pagina-acomodações.jsx'
-import PaginaDomo from './Pages/Pagina-Dome/Pagina-Dome.jsx'
-import PaginaContato from './Pages/Pagina-Contato/Pagina-Contato.jsx'
+import PaginaLogin from "./Dashebord/Pages/Pagina_login/Pagina_login.jsx"
 /// teste
 function App() {
  return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<PaginaIncial/>}></Route>
-        <Route path='/Contato' element={<PaginaContato/>}></Route>
-        <Route path='/Acomodacoes' element={<PaginaAcomodacoes/>}></Route>
-        <Route path='acomodacao/Domo' element={<PaginaDomo/>}></Route>
-        <Route path='Domo' element={<PaginaDomo/>}></Route>
+        <Route path='/' element={<PaginaLogin/>}></Route>
+      
       </Routes>
     </BrowserRouter>
   )
---
+++ src/Dashebord/Pages/Pagina_login/Pagina_login.jsx
@@ -0,0 +1,12 @@
+import Box_login from "../../Components/Box_login/Box_login.jsx"
+
+
+const PaginaLogin = () => {
+    return(
+        <>
+            <Box_login/>
+        </>
+    )
+}
+
+export default Box_login
