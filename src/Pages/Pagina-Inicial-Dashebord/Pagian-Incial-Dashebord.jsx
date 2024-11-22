1f4a7fe41583a8b94658e50a4829b8573f642476
arthur972645 <arthurall.1973@gmail.com>
2024-11-21 22:12:01 -0300

    animação de transição entre as paginas de login e a pagina inicial

--- src/Pages/Pagina-Inicial-Dashebord/Pagian-Incial-Dashebord.jsx
+++ src/Pages/Pagina-Inicial-Dashebord/Pagian-Incial-Dashebord.jsx
@@ -1,15 +1,22 @@
-import ReservaDashebord from "../../Components/ReservaDashebord/ResrevaDashebord"
-import Navbar from "../../Components/NavbarDashebord/NavbarDashebord.jsx"
-import LucoMensal from "../../Components/LucroMensalFinanceiroDashebord/LucroMensalFinanceiroDashebord.jsx"
+import { motion } from "framer-motion";
+import ReservaDashebord from "../../Components/ReservaDashebord/ResrevaDashebord";
+import Navbar from "../../Components/NavbarDashebord/NavbarDashebord.jsx";
+import LucoMensal from "../../Components/LucroMensalFinanceiroDashebord/LucroMensalFinanceiroDashebord.jsx";
 
 const PaginaInicialDashebord = () => {
-  return(
-    <>
-    <Navbar/>
-    <ReservaDashebord/>
-    <LucoMensal/>
-    </>
-      
-  )
-}
-export default PaginaInicialDashebord
+  return (
+    <motion.div
+      className="dashboard"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -50 }}
+      transition={{ duration: 1 }}
+    >
+      <Navbar />
+      <ReservaDashebord />
+      <LucoMensal />
+    </motion.div>
+  );
+};
+
+export default PaginaInicialDashebord;
